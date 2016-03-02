7cf91ca184ea2d01b969deab7cb73fa2980bef50
Morten <morteano@stud.ntnu.no>
2016-03-02 09:27:15 +0100

    add super functions in subclass constructors

--- app/src/main/java/com/magicaeludos/mobile/magicaeludos/implementation/Player.java
+++ app/src/main/java/com/magicaeludos/mobile/magicaeludos/implementation/Player.java
@@ -5,10 +5,6 @@ import android.graphics.Bitmap;
 public class Player extends GameObject {
 
     public Player(double x, double y, int width, int height, Bitmap sprite){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.sprite = sprite;
+        super(x, y, width, height, sprite);
     }
 }
