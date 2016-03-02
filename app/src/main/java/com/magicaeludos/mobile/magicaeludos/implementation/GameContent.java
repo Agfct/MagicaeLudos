8e693efeb78226184de424f7ea1282bc297af028
AndersLunde <agfct@hotmail.com>
2016-03-02 10:06:15 +0100

    new branch add canvas.drawText();

--- app/src/main/java/com/magicaeludos/mobile/magicaeludos/implementation/GameContent.java
+++ app/src/main/java/com/magicaeludos/mobile/magicaeludos/implementation/GameContent.java
@@ -1,4 +1,6 @@
 package com.magicaeludos.mobile.magicaeludos.implementation;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
